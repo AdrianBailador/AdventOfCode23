2e90fc7f8cf5ce88272eabb6996e1d2937ab82e6
Adrián Bailador Panero <adrianbailador@hotmail.com>
2023-12-02 13:30:21 +0000

    Add project files.

---
+++ CubeConundrum/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace CubeConundrum
+{
+    class Program
+    {
+        static void Main()
+        {
+            string path = "C:\\Users\\Adrián Bailador\\Desktop\\Day2.txt";
+            string content = File.ReadAllText(path);
+            string[] lines = content.Split('\n');
+
+            int sumGameIds = CalculateSumOfGameIds(lines);
+            Console.WriteLine("Day 2: Cube Conundrum (Part 1): The sum of the IDs of the possible games is: " + sumGameIds);
+
+            int sumPower = CalculateSumOfPower(lines);
+            Console.WriteLine("Day 2: Cube Conundrum (Part 2): The sum of the power of the minimum set of cubes is: " + sumPower);
+        }
+
+        static Dictionary<string, int> ProcessRound(string round)
+        {
+            var colorCounts = new Dictionary<string, int> { { "red", 0 }, { "green", 0 }, { "blue", 0 } };
+            var cubes = round.Split(',');
+            foreach (var cube in cubes)
+            {
+                var parts = cube.Trim().Split(' ');
+                var count = int.Parse(parts[0]);
+                var color = parts[1];
+                colorCounts[color] = Math.Max(colorCounts[color], count);
+            }
+            return colorCounts;
+        }
+
+        static int CalculateSumOfGameIds(string[] lines)
+        {
+            int sum = 0;
+            foreach (string line in lines)
+            {
+                var gameParts = line.Split(':');
+                var gameId = int.Parse(gameParts[0].Replace("Game ", ""));
+                var rounds = gameParts[1].Split(';');
+
+                bool isPossible = true;
+                foreach (var round in rounds)
+                {
+                    var colorCounts = ProcessRound(round);
+                    if (colorCounts["red"] > 12 || colorCounts["green"] > 13 || colorCounts["blue"] > 14)
+                    {
+                        isPossible = false;
+                        break;
+                    }
+                }
+
+                if (isPossible)
+                {
+                    sum += gameId;
+                }
+            }
+            return sum;
+        }
+
+        static int CalculateSumOfPower(string[] lines)
+        {
+            int sumPower = 0;
+            foreach (string line in lines)
+            {
+                var gameParts = line.Split(':');
+                var rounds = gameParts[1].Split(';');
+
+                int maxRed = 0, maxGreen = 0, maxBlue = 0;
+                foreach (var round in rounds)
+                {
+                    var colorCounts = ProcessRound(round);
+                    maxRed = Math.Max(maxRed, colorCounts["red"]);
+                    maxGreen = Math.Max(maxGreen, colorCounts["green"]);
+                    maxBlue = Math.Max(maxBlue, colorCounts["blue"]);
+                }
+
+                sumPower += maxRed * maxGreen * maxBlue;
+            }
+            return sumPower;
+        }
+    }
+}
---
+++ Trebuchet/Program.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+class Trebuchet
+{
+    static void Main()
+    {
+        string content = File.ReadAllText("C:\\Users\\Adrián Bailador\\Desktop\\Day1.txt");
+        Console.WriteLine("Day 1! task 1");
+        Console.WriteLine(TaskOne(content));
+        Console.WriteLine("Day 1! task 2");
+        Console.WriteLine(TaskTwo(content));
+    }
+
+    static string TaskOne(string content)
+    {
+        var lines = content.Split('\n');
+        int totalNumber = 0;
+
+        foreach (var line in lines)
+        {
+            var numberCharacters = line.Where(char.IsDigit).ToList();
+            if (numberCharacters.Count > 1)
+            {
+                var stringNumber = numberCharacters.First().ToString() + numberCharacters.Last().ToString();
+                totalNumber += int.Parse(stringNumber);
+            }
+        }
+
+        return totalNumber.ToString();
+    }
+
+    static string TaskTwo(string content)
+    {
+        var lines = content.Split('\n');
+        int totalNumber = 0;
+        var numbersAsText = new[] { "one", "two", "three", "four", "five", "six", "seven", "eight", "nine" };
+        var numbersList = new[] { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+
+        foreach (var line in lines)
+        {
+            var firstNumber = FindFirst(numbersAsText, numbersList, line);
+            var lastNumber = FindLast(numbersAsText, numbersList, line);
+            var stringNumber = $"{firstNumber}{lastNumber}";
+            totalNumber += int.Parse(stringNumber);
+        }
+
+        return totalNumber.ToString();
+    }
+
+    static int FindFirst(string[] numbersAsText, string[] numbersList, string line)
+    {
+        int lowestIteration = line.Length;
+        string currentUsedValue = "";
+
+        foreach (var currentNumber in numbersAsText.Concat(numbersList))
+        {
+            var firstIndex = line.IndexOf(currentNumber);
+            if (firstIndex != -1 && firstIndex <= lowestIteration)
+            {
+                lowestIteration = firstIndex;
+                currentUsedValue = currentNumber;
+            }
+        }
+
+        return ConvertToNumber(currentUsedValue);
+    }
+
+    static int FindLast(string[] numbersAsText, string[] numbersList, string line)
+    {
+        int highestIteration = 0;
+        string currentUsedValue = "";
+
+        foreach (var currentNumber in numbersAsText.Concat(numbersList))
+        {
+            var lastIndex = line.LastIndexOf(currentNumber);
+            if (lastIndex != -1 && lastIndex >= highestIteration)
+            {
+                highestIteration = lastIndex;
+                currentUsedValue = currentNumber;
+            }
+        }
+
+        return ConvertToNumber(currentUsedValue);
+    }
+
+    static int ConvertToNumber(string number)
+    {
+        return number switch
+        {
+            "one" => 1,
+            "two" => 2,
+            "three" => 3,
+            "four" => 4,
+            "five" => 5,
+            "six" => 6,
+            "seven" => 7,
+            "eight" => 8,
+            "nine" => 9,
+            "1" => 1,
+            "2" => 2,
+            "3" => 3,
+            "4" => 4,
+            "5" => 5,
+            "6" => 6,
+            "7" => 7,
+            "8" => 8,
+            "9" => 9,
+            _ => throw new Exception($"ERROR {number}")
+        };
+    }
+}
